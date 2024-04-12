188f87caba4a069d23a582685a2cd5107018845a
Filipe Paisana <20220843@iade.pt>
2024-04-12 10:39:29 +0100

    Snackbar adionada

--- Web/src/Registo.js
+++ Web/src/Registo.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
-import { TextField, Button, Typography, Container, Box, CssBaseline } from '@mui/material';
+import { TextField, Button, Typography, Container, Box, CssBaseline, Snackbar, Alert, Slide } from '@mui/material';
+
+function TransitionLeft(props) {
+  return <Slide {...props} direction="left" />;
+}
 
 function Registo() {
   const [nome, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState('success');
 
   function handleSubmit(e) {
     e.preventDefault();
-
     const userData = { nome, email, password };
 
     fetch('http://localhost:4000/users', {
@@ -18,72 +24,107 @@ function Registo() {
       },
       body: JSON.stringify(userData),
     })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Erro ao registrar:', error));
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      setName('');
+      setEmail('');
+      setPassword('');
+      throw new Error('Algo deu errado no registro!');
+
+    })
+    .then(data => {
+      console.log(data);
+      setSnackbarMessage('Registro realizado com sucesso!');
+      setSnackbarSeverity('success');
+      setOpenSnackbar(true);
+      
+      setName('');
+      setEmail('');
+      setPassword('');
+    })
+    .catch(error => {
+      console.error('Erro ao registrar:', error);
+      setSnackbarMessage(error.message);
+      setSnackbarSeverity('error');
+      setOpenSnackbar(true);
+
+    });
   }
+
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSnackbar(false);
+  };
 
   return (
     <Container component="main" maxWidth="xs">
-      <div className="content"></div>
-      <CssBaseline />
-      <Box
+      <div className ="content">
+        <CssBaseline />
+        <Box sx={{ marginTop: 12, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Typography component="h1" variant="h5">Registo</Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="nome"
+              label="Nome"
+              name="nome"
+              autoComplete="nome"
+              autoFocus
+              value={nome}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Endereço de Email"
+              name="email"
+              autoComplete="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Registar
+            </Button>
+          </Box>
+        </Box>
+      </div>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        TransitionComponent={TransitionLeft}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
         sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          '& .MuiAlert-root': {
+            width: '100%',
+            backgroundColor: snackbarSeverity === 'success' ? '#b7dc18' : '#ff0000',
+          }
         }}
       >
-        <Typography component="h1" variant="h5">
-          Registo
-        </Typography>
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="nome"
-            label="Nome"
-            name="nome"
-            autoComplete="nome"
-            autoFocus
-            value={nome}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Endereço de Email"
-            name="email"
-            autoComplete="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Senha"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            >
-            Registar
-          </Button>
-        </Box>
-      </Box>
+        <Alert onClose={handleCloseSnackbar} severity={snackbarSeverity}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </Container>
   );
 }
