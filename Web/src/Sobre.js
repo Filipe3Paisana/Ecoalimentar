254e52af084be0cc190682a828f789f75d52c4be
Filipe Paisana <20220843@iade.pt>
2024-04-21 02:13:30 +0100

    Melhorar apresentação do Sobre nós

--- Web/src/Sobre.js
+++ Web/src/Sobre.js
@@ -8,19 +8,50 @@ function ConsultoriaPage() {
   return (
     <>
     <div className="content">
-      <h1>Consultoria</h1>
-      <p>Bem-vindo à nossa página de consultoria!</p>
-      <p>
-        Aqui, oferecemos consultoria especializada em várias áreas, incluindo, mas não limitado a:
+      <h1>Sobre nós</h1>
+      <br>
+      </br>
+      <h2>Missão</h2>
+      <p> missão da Ecoalimentar é prestar serviços de consultoria com valor acrescentado para os clientes e parceiros, nas áreas da Segurança Alimentar, Gestão da Qualidade, Higiene e Segurança no Trabalho, Gestão Ambiental, e Formação.
+Criar soluções para as empresas, desenvolvendo o seu potencial de negócio de forma simples, integrada, sustentável e direcionada para o futuro.</p>
+      <br></br>
+      <h2>Visão</h2>
+      <p> Ser uma empresa reconhecida internacionalmente pela sua capacidade de criar sistemas de gestão práticos, criativos e sustentáveis que espelhem a energia própria da identidade de cada empresa.</p>
+      <br></br>
+
+      <h2>Valores</h2>
+
+      <br></br>
+      <p>Competencia e transparecia:
+        <ul>
+          <li>Ética e integridade</li>
+          <li>Profissionalismo e rigor</li>
+          <li>Flexibilidade e inovação</li>
+          <li>Confiança e transparência</li>
+          <li>Responsabilidade social e ambiental</li>
+        </ul>
       </p>
-      <ul>
-        <li>Consultoria empresarial</li>
-        <li>Consultoria tecnológica</li>
-        <li>Consultoria de marketing</li>
-        <li>Consultoria financeira</li>
-      </ul>
-      <p>
-        Entre em contato conosco para saber mais sobre como podemos ajudar o seu negócio a crescer e prosperar.
+      <br></br>
+      <p>Confiança:
+        <ul>
+          <li>Estabelecer relações duradouras</li>
+          <li>- Criar empatia;</li>
+          <li>Criar compromissos</li>
+        </ul>
+      </p>
+      <br></br>
+      <p>Orientação para o cliente:
+        <ul>
+          <li>Compreender a cultura e os requisitos do client</li>
+          <li>Procurar satisfazer e exceder as expectativas do cliente.</li>        
+        </ul>
+      </p>
+      <br></br>
+      <p>atualização e criatividade:
+        <ul>
+          <li> Desenvolver competências para as necessidades solicitadas </li>
+          <li>omentar a iniciativa e a criatividade, para desenvolver e inovar permanentemente os nossos serviços</li>
+        </ul>
       </p>
     </div>
     <Footer />
