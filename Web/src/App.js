a8dbca6e4f24f4957e2587f6cdf614f0f1f00e19
Filipe Paisana <20220843@iade.pt>
2024-04-21 19:10:48 +0100

    Alteração do path da Home(correção)

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./";
+import Home from "./Home";
 import Consultoria from "./Consultoria";
 import Auditoria from "./Auditoria";
 import Formacao from "./Formacao";
