2975c195433acf35de1516af3868d6dc8d81f9ab
Filipe Paisana <20220843@iade.pt>
2024-04-23 18:30:05 +0100

    Melhoria no Auditoria e na Consultoria

--- Web/src/Auditoria.js
+++ Web/src/Auditoria.js
@@ -10,12 +10,25 @@ function Auditoria() {
     <>
     <div className="content">
       <h1>Auditoria</h1>
-      <p>Aqui você encontrará informações sobre nossos serviços de auditoria.</p>
+      <br></br>
+      <p>
+        A qualidade e segurança alimentar são, atualmente, preocupações constantes para os consumidores e administração pública. Nesta envolvente de pressão é crucial controlar e melhorar o desempenho dos produtos, serviços e processos, de forma a manter a competitividade e proteger a imagem.
+      </p>
+      <p>
+        Desta forma, o uso de planos de auditoria é um importante mecanismo adoptado em sistemas da qualidade nas empresas durante o ciclo produtivo. As auditorias permitem, através da visão e experiência de peritos, detectar problemas reais que, por serem quotidianos, podem passar despercebidos à própria empresa. Adicionalmente, as auditorias representam uma entrada de informação e uma visão distinta da realizada pela empresa. As auditorias são, também, ferramentas de melhoria que permitem à empresa conhecer a sua realidade e implementar medidas para melhorar o seu desempenho.
+      </p>
+      <p>
+        De modo a dar resposta às necessidades das empresas, nomeadamente no cumprimento da legislação afecta à segurança alimentar, a Ecoalimentar disponibiliza os seguintes serviços de auditorias:
+      </p> 
       <ul>
-        <li>Auditoria financeira</li>
-        <li>Auditoria de sistemas</li>
-        <li>Auditoria de processos</li>
+        <li>- Auditorias internas: BRCGS, IFS, FSSC 22000, ISO 22000, ISO 9001, Global G.A.P., entre outras.</li>
+        <li>- Inspeções ambiente de trabalho, higiene pessoal e instalações;</li>
+        <li>- Auditorias de levantamento de necessidades;</li>
+        <li>- Auditorias de revisão e acompanhamento aos sistemas HACCP;</li>
+        <li>- Auditorias integradas de Sistemas ISO - Qualidade, Ambiente e Segurança;</li>
+        <li>- Auditorias Externas a fornecedores e/ou distribuidores.</li>
       </ul>
+      <br></br>
       <p>Entre em contato conosco para mais informações sobre nossos serviços de auditoria.</p>
       <Button variant="contained" component={Link} to="/contactos">
           Entrar em Contacto
--- Web/src/Consultoria.js
+++ Web/src/Consultoria.js
@@ -11,13 +11,45 @@ function Consultoria() {
     <div className="content">
     <Container component="main" maxWidth="md">
         <h1>Consultoria</h1>
-        <p>Bem-vindo à nossa página de consultoria!</p>
-        <p>Aqui, oferecemos consultoria especializada em várias áreas, incluindo, mas não limitado a:</p>
+        <br></br>
+        <p>As empresas necessitam de cumprir a legislação em vigor para poderem continuar as suas atividades. No entanto, para quem pretende expandir o seu negocio ou mesmo manter-se competitivo face aos seus concorrentes, há sempre algo mais a acrescentar.Para tal é necessário por um lado ter um perfeito conhecimento do seu negocio (possível através de uma análise sistemática por indicadores) e por outro manter-se criativo por forma a posicionar-se corretamente adivinhando as expectativas e necessidades dos seus clientes.De acordo com a fase de desenvolvimento da sua empresa terá necessidade de serviços distintos. Desta forma propomos uma prestação de serviços de forma integrada com o funcionamento da empresa, sob a forma de módulos, os quais serão implementados de acordo com as necessidades do cliente.</p>
+        <br/>
+        <p>Implementação dos seguintes referenciais:</p>
         <ul>
-          <li>Consultoria empresarial</li>
-          <li>Consultoria tecnológica</li>
-          <li>Consultoria de marketing</li>      
-          <li>Consultoria financeira</li>
+          <li>- IFS - International Food Standard;</li>
+          <li>- BRCG - British Retail Consortium Global Standard;</li>
+          <li>- FSSC 22000 - Food Safety System Certification;</li>      
+          <li>- GLOBAL G.A.P.;</li>
+          <li>- GRASP;</li>
+          <li>- Sistemas HACCP (Codex Alimentarius, Regulamentos n.º 852/2004 e n.º 853/2004);</li>
+          <li>- Sistemas de Gestão da Qualidade (ISO 9001);</li>
+          <li>- Sistemas de Gestão da Segurança Alimentar (ISO 22000);</li>
+          <li>- Sistemas de Gestão Ambiental (ISO 14001);</li>
+          <li>- Sistemas de Gestão de Segurança e Higiene no Trabalho (ISO 45001);</li>
+          <li>• Acompanhamento / Manutenção de Sistemas de Gestão da Qualidade e de Segurança Alimentar (Outsourcing);</li>
+          <li>• Apoio no cumprimento da legislação vigente aplicável.</li>
+        </ul>
+        <br/>
+        <p>Prestação de serviços modulares:</p>
+        <ul>
+          <li>- Implementação de Sistemas HACCP (Codex Alimentarius, Regulamentos n.º 852/2004 e n.º 853/2004);</li>
+          <li>- Gestão de ocurrencias</li>
+          <li>- Gestção de indicadores </li>
+          <li>- Gestão de plano de análise</li>  
+          <li>- Gestão de plano de Formação</li>
+          <li>- Indicadores de qualidade de serviço;</li>
+          <li>- Gestão documental;</li>
+          <li>- Organização e manutenção da legislação adequada ao sector;</li>
+          <li>- Qualificação de fornecedores;</li>
+          <li>- Validação de rotulagem;</li>
+          <li>- Validação de manuais;</li>
+          <li>- Validação de produto;</li>
+          <p>- Optimização do Processo de importação de produtos fora da UE:</p>
+            <ul>
+              <li>- inspeção pré-embarque;</li>
+              <li>- Teste e certificados aplicaveis;</li>
+              <li>- Ensaios;</li>
+            </ul>
         </ul>
         <p>Entre em contato conosco para saber mais sobre como podemos ajudar o seu negócio a crescer e prosperar.</p>
       </Container>
